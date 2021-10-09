6cf17909cec82777db71aa228e109350deadc92d
AndreG-P <andre.greiner-petter@t-online.de>
2021-10-09 19:34:40 +0900

    First license safe artifact of lacast

---
+++ lacast-eval-symbolic.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+LACAST_BIN="./bin"
+LACAST_LIBS="./libs"
+LACAST_CLASSPATH=
+
+for JAR in "${LACAST_LIBS}"/*.jar
+do
+    LACAST_CLASSPATH="${LACAST_CLASSPATH}:${JAR}"
+done
+
+for JAR in "${LACAST_BIN}"/*.jar
+do
+    LACAST_CLASSPATH="${LACAST_CLASSPATH}:${JAR}"
+done
+
+# set environment vars
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/opt/maple2020/bin.X86_64_LINUX"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/opt/Wolfram/SystemFiles/Links/JLink/SystemFiles/Libraries/Linux-x86-64/"
+export MAPLE="/opt/maple2020"
+
+eval java -Xmx15g -Xss100M -cp "$LACAST_CLASSPATH" gov.nist.drmf.interpreter.evaluation.core.symbolic.SymbolicEvaluator "$@"
