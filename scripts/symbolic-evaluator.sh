3b472596ee722b90e5492709cc8c6595f6706c35
AndreG-P <andre.greiner-petter@t-online.de>
2021-10-16 01:22:15 +0900

    First round of new symbolic results with mathematica

--- scripts/symbolic-evaluator.sh
+++ scripts/symbolic-evaluator.sh
@@ -41,6 +41,13 @@ while [[ $# -gt 0 ]]; do
         exit 1;
       elif [[ ! -d "${lacast_cas_Mathematica_native_library_path}" ]]; then
         echo "ERROR: The specified library path in lacast.config.yaml for Mathematica does not exist (${lacast_cas_Mathematica_native_library_path})"
+        exit 1;
+      elif [[ ! -f "${lacast_libs_path}/JLink.jar" ]]; then
+        echo "Unable to find necessary JLink.jar in LaCASt's libs folder."
+        echo "Copy it from your Mathematica install path. You probably find it under "
+        echo "  ${lacast_cas_Mathematica_install_path}/SystemFiles/Links/JLink/JLink.jar"
+        echo ""
+        exit 1;
       else
         export LD_LIBRARY_PATH="${lacast_cas_Mathematica_native_library_path}"
       fi
@@ -59,6 +66,12 @@ while [[ $# -gt 0 ]]; do
         exit 1;
       elif [[ ! -d "${lacast_cas_Maple_install_path}" ]]; then
         echo "ERROR: The specified install path of Maple in lacast.config.yaml does not exist (${lacast_cas_Maple_install_path})"
+        exit 1;
+      elif [[ ! -f "${lacast_libs_path}/Maple.jar" || ! -f "${lacast_libs_path}/externalcall.jar" ]]; then
+        echo "Unable to find necessary Maple.jar and externalcall.jar in LaCASt's libs folder."
+        echo "Copy it from your Maple install path. You most likely find them in:"
+        echo "  ${lacast_cas_Mapla_install_path}/Java"
+        echo ""
         exit 1;
       else
         export LD_LIBRARY_PATH="${lacast_cas_Maple_native_library_path}"
--- scripts/test-maple.sh
+++ scripts/test-maple.sh
@@ -5,7 +5,24 @@ source ./scripts/parser.sh
 eval $(parse_yaml lacast.config.yaml)
 eval $(load_jars)
 
-export LD_LIBRARY_PATH="${lacast_cas_Maple_native_library_path}"
-export MAPLE="${lacast_cas_Maple_install_path}"
+if [[ -z "${lacast_cas_Maple_native_library_path}" || -z "${lacast_cas_Maple_install_path}" ]]; then
+	echo "ERROR: You want to use Maple for symbolic evaluation but did not specify valid paths in lacast.config.yaml"
+	exit 1;
+elif [[ ! -d "${lacast_cas_Maple_native_library_path}" ]]; then
+	echo "ERROR: The specified library path of Maple in lacast.config.yaml does not exist (${lacast_cas_Maple_native_library_path})"
+	exit 1;
+elif [[ ! -d "${lacast_cas_Maple_install_path}" ]]; then
+	echo "ERROR: The specified install path of Maple in lacast.config.yaml does not exist (${lacast_cas_Maple_install_path})"
+	exit 1;
+elif [[ ! -f "${lacast_libs_path}/Maple.jar" || ! -f "${lacast_libs_path}/externalcall.jar" ]]; then
+	echo "Unable to find necessary Maple.jar and externalcall.jar in LaCASt's libs folder."
+	echo "Copy it from your Maple install path. You most likely find them in:"
+	echo "  ${lacast_cas_Mapla_install_path}/Java"
+	echo ""
+	exit 1;
+else
+	export LD_LIBRARY_PATH="${lacast_cas_Maple_native_library_path}"
+	export MAPLE="${lacast_cas_Maple_install_path}"
+fi
 
 eval java -cp "$LACAST_CLASSPATH" -Xss50M gov.nist.drmf.interpreter.maple.MapleConnectorSimpleTester
